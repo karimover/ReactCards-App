dd8432f57d6156bfff1721885a6d9c0ba26593c9
Emir Karimov <karimover06@gmail.com>
2026-04-05 13:46:33 +0500

    home page, question card

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { MainLayout } from "./components/MainLayout";
+import { HomePage } from "./pages/HomePage";
 
 function App() {
     // return <MainLayout />;
@@ -7,7 +8,7 @@ function App() {
         <BrowserRouter>
             <Routes>
                 <Route element={<MainLayout />}>
-                    <Route path="/" element={<div>home</div>} />
+                    <Route path="/" element={<HomePage />} />
                     <Route path="/main" element={<div>main component</div>} />
                     <Route path="/addquestion" element={<div>add question</div>} />
                     <Route path="/forbidden" element={<div>forbidden</div>} />
