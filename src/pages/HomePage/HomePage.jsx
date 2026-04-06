5a67834b2e5d59073c0da513308d6231949b2ac0
Emir Karimov <karimover06@gmail.com>
2026-04-06 17:14:10 +0500

    useEffect, move db, api_url

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,43 +1,30 @@
+import { useState, useEffect } from "react";
 import { QuestionCard } from "../../components/QuestionCard/QuestionCard";
+import { API_URL } from "../../constants";
 // import cls from "./HomePage.module.css";
 
-const cards = [
-    {
-        id: "6",
-        question: "Что такое состояние (state) в React?",
-        answer: "State — это внутренние данные компонента, которые могут изменяться.",
-        description:
-            "Состояние управляется внутри компонента и изменяется через `setState` в классовых компонентах или `useState` в функциональных. Изменение состояния инициирует повторный рендер компонента.",
-        resources: ["https://react.dev/learn/state-a-components-memory"],
-        level: 2,
-        completed: true,
-    },
-    {
-        id: "7",
-        question: "Как работает useState в React?",
-        answer: "useState — это хук для управления состоянием в функциональных компонентах.",
-        description:
-            "`useState` возвращает массив из двух элементов: текущего значения состояния и функции для его обновления. Используется для хранения и изменения данных в компонентах.",
-        resources: ["https://react.dev/reference/react/useState"],
-        level: 2,
-        completed: true,
-    },
-    {
-        id: "8",
-        question: "Что такое useEffect?",
-        answer: "useEffect — это хук для управления побочными эффектами.",
-        description:
-            "Хук `useEffect` позволяет выполнять побочные эффекты, такие как запросы к API, подписки и манипуляции с DOM. Он вызывается после рендеринга и может зависеть от определенных значений.",
-        resources: ["https://react.dev/reference/react/useEffect"],
-        level: 2,
-        completed: true,
-    },
-];
-
 export const HomePage = () => {
+    const [questions, setQuestions] = useState([]);
+
+    const getQuestions = async () => {
+        try {
+            const response = await fetch(`${API_URL}/react`);
+            const questions = await response.json();
+            setQuestions(questions);
+            console.log("questions", questions);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        // eslint-disable-next-line react-hooks/set-state-in-effect
+        getQuestions();
+    }, []);
+
     return (
         <>
-            {cards.map((card, index) => {
+            {questions.map((card, index) => {
                 return <QuestionCard card={card} key={index} />;
             })}
         </>
