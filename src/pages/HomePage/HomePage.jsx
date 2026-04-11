591ccede7a0ad17651762c988099ed6ece9c1248
Emir Karimov <karimover06@gmail.com>
2026-04-11 15:38:56 +0500

    sort logic

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { API_URL } from "../../constants";
 import { QuestionCardList } from "../../components/QuestionCardList/";
 import { Loader } from "../../components/Loader";
@@ -9,39 +9,55 @@ import { SearchInput } from "../../components/SearchInput";
 export const HomePage = () => {
     const [questions, setQuestions] = useState([]);
     const [searchValue, setSearchValue] = useState("");
+    const [sortSelectValue, setSortSelectValue] = useState("");
 
     const [getQuestions, isLoading, error] = useFetch(async (url) => {
         const response = await fetch(`${API_URL}/${url}`);
+        const questions = await response.json();
 
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
-        
-        const questions = await response.json();
+
         setQuestions(questions);
         return questions;
     });
 
+    const cards = useMemo(() => {
+        return questions.filter((d) => d.question.toLowerCase().includes(searchValue.trim().toLocaleLowerCase()));
+    }, [questions, searchValue]);
+
     useEffect(() => {
-        getQuestions("react");
-    }, []);
+        getQuestions(`react?${sortSelectValue}`);
+    }, [sortSelectValue]);
 
     const onSearchChangeHandler = (e) => {
-        console.log(e.target.value);
         setSearchValue(e.target.value);
-    }
+    };
 
-
+    const onSortSelectChangeHandler = (e) => {
+        setSortSelectValue(e.target.value);
+    };
     return (
         <>
             <div className={cls.controlsContainer}>
-                <SearchInput value={searchValue} onChange={onSearchChangeHandler}/>
+                <SearchInput value={searchValue} onChange={onSearchChangeHandler} />
+
+                <select value={sortSelectValue} onChange={onSortSelectChangeHandler} className={cls.select}>
+                    <option value="">sort by</option>
+                    <hr />
+                    <option value="_sort=level">level ASC</option>
+                    <option value="_sort=-level">level DESC</option>
+                    <option value="_sort=completed">completed ASC</option>
+                    <option value="_sort=-completed">completed DESC</option>
+                </select>
             </div>
-            
 
             {isLoading && <Loader />}
             {error && <p>{error}</p>}
-            <QuestionCardList cards={questions} />
+            {cards.length === 0 && <p className={cls.noCardsInfo}>No cards...</p>}
+
+            <QuestionCardList cards={cards} />
         </>
     );
 };
