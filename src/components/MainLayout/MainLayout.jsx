6ab081436b9d46abbdd7fde03c5cc569636ffc36
Emir Karimov <karimover06@gmail.com>
2026-04-21 14:45:18 +0500

    react toasts

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,22 +1,27 @@
 import { Outlet } from "react-router-dom";
 import cls from "./MainLayout.module.css";
 import { Header } from "../Header";
+import { ToastContainer } from "react-toastify";
 
 export const MainLayout = () => {
     const currentYear = new Date().getFullYear();
 
     return (
-        <div className={cls.mainLayout}>
-            <Header />
-            <div className={cls.mainWrapper}>
-                <main className={cls.main}>
-                    <Outlet />
-                </main>
-                <footer className={cls.footer}>
-                    React Question Cards Application | {currentYear} <br />
-                    by Karimov Emir
-                </footer>
+        <>
+            <div className={cls.mainLayout}>
+                <Header />
+                <div className={cls.mainWrapper}>
+                    <main className={cls.main}>
+                        <Outlet />
+                    </main>
+                    <footer className={cls.footer}>
+                        React Question Cards Application | {currentYear} <br />
+                        by Karimov Emir
+                    </footer>
+                </div>
             </div>
-        </div>
+
+            <ToastContainer />
+        </>
     );
 };
