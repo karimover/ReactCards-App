d2646eb393036e989a9112611d450bfe7fec724c
Emir Karimov <karimover06@gmail.com>
2026-05-01 16:34:13 +0500

    add question page layout

--- src/pages/QuestionPage/QuestionPage.jsx
+++ src/pages/QuestionPage/QuestionPage.jsx
@@ -42,6 +42,7 @@ export const QuestionPage = () => {
     }, []);
 
     useEffect(() => {
+        // eslint-disable-next-line react-hooks/set-state-in-effect
         card !== null && setIsChecked(card.completed);
     }, [card]);
     const onCheckboxChangeHandler = () => {
