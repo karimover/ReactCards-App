8c5a32705404ca9279621ee3dd35018b57793d65
Emir Karimov <karimover06@gmail.com>
2026-05-06 22:47:19 +0500

    add new question logic

--- src/pages/AddQuestionPage/AddQuestionPage.jsx
+++ src/pages/AddQuestionPage/AddQuestionPage.jsx
@@ -25,12 +25,17 @@ const createCardAction = async (_prevState, formData) => {
                 editDate: undefined,
             }),
         });
+
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
         const question = response.json();
         toast.success("New question is successfully created!");
 
         return isClearForm ? {} : question;
     } catch (error) {
         toast.error(error.message);
+        return {};
     }
 };
 
