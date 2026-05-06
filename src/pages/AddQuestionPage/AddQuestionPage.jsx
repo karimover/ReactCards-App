1d1b9ed2b86dcd600ea88755fd14fe019f0a27b5
Emir Karimov <karimover06@gmail.com>
2026-05-06 22:51:00 +0500

    add question loader and fix resources

--- src/pages/AddQuestionPage/AddQuestionPage.jsx
+++ src/pages/AddQuestionPage/AddQuestionPage.jsx
@@ -1,5 +1,6 @@
 import { useActionState } from "react";
 import { Button } from "../../components/Button";
+import { Loader } from "../../components/Loader";
 import cls from "./AddQuestionPage.module.css";
 import { delayFn } from "../../helpers/delayFn";
 import { toast } from "react-toastify";
@@ -44,6 +45,7 @@ export const AddQuestionPage = () => {
 
     return (
         <>
+            {isPending && <Loader />}
             <h1 className={cls.formTitle}>Add new question</h1>
 
             <div className={cls.formContainer}>
@@ -92,7 +94,6 @@ export const AddQuestionPage = () => {
                             id="resourcesField"
                             cols="30"
                             rows="5"
-                            required
                             placeholder="please enter resources separated by commas"
                         ></textarea>
                     </div>
