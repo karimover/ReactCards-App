7c2d29da2ab970806fe96d7184d9d111570e17d0
Emir Karimov <karimover06@gmail.com>
2026-06-02 20:08:08 +0500

    auth provider, protected routes

---
+++ src/auth/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,12 @@
+import { createContext, useState } from "react";
+import { AUTH_STORAGE } from "../../constants";
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+    const isLogin = JSON.parse(localStorage.getItem(AUTH_STORAGE)) || false;
+    const [isAuth, setIsAuth] = useState(isLogin);
+
+    return <AuthContext.Provider value={{ isAuth, setIsAuth }}> {children} </AuthContext.Provider>;
+};
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,19 @@
 import { useNavigate } from "react-router-dom";
 import ReactLogo from "../../assets/react.svg";
 import { Button } from "../Button";
+import { useAuth } from "../../hooks/useAuth";
 import cls from "./Header.module.css";
+import { AUTH_STORAGE } from "../../constants";
 
 export const Header = () => {
     const navigate = useNavigate();
+    const { isAuth, setIsAuth } = useAuth();
+
+    const loginHandler = () => {
+        localStorage.setItem(AUTH_STORAGE, !isAuth);
+        setIsAuth(!isAuth);
+    };
+
     return (
         <header className={cls.header}>
             <p onClick={() => navigate("/")}>
@@ -13,8 +22,10 @@ export const Header = () => {
             </p>
 
             <div className={cls.headerButtons}>
-                <Button onClick={() => navigate("/addquestion")}>Add</Button>
-                <Button>Login</Button>
+                {isAuth && <Button onClick={() => navigate("/addquestion")}>Add</Button>}
+                <Button onClick={loginHandler} isActive={!isAuth}>
+                    {isAuth ? "Logout" : "Login"}
+                </Button>
             </div>
         </header>
     );
