0b4ab09fbb64444901e0c8c06cd4debcc94738b5
Emir Karimov <karimover06@gmail.com>
2026-06-03 18:24:57 +0500

    theme context, theme toggler, darkLayout class

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import { Button } from "../Button";
 import { useAuth } from "../../hooks/useAuth";
 import cls from "./Header.module.css";
 import { AUTH_STORAGE } from "../../constants";
+import { ThemeToggler } from "../../features/ThemeToggler";
 
 export const Header = () => {
     const navigate = useNavigate();
@@ -22,6 +23,8 @@ export const Header = () => {
             </p>
 
             <div className={cls.headerButtons}>
+                <ThemeToggler />
+
                 {isAuth && <Button onClick={() => navigate("/addquestion")}>Add</Button>}
                 <Button onClick={loginHandler} isActive={!isAuth}>
                     {isAuth ? "Logout" : "Login"}
