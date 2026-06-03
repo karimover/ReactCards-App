9d8b70016853cebf5d7db4d344201551eb036752
Emir Karimov <karimover06@gmail.com>
2026-06-03 19:10:00 +0500

    server branch init

--- src/components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import cls from "./Button.module.css";
-
-export const Button = ({ onClick, isActive, isDisabled, children }) => {
-    return (
-        <button className={`${cls.btn} ${isActive ? cls.active : ""}`} onClick={onClick} disabled={isDisabled}>
-            {children}
-        </button>
-    );
-};
--- src/pages/NotFoundPage/NotFoundPage.jsx
+++
@@ -1,5 +0,0 @@
-import cls from "./NotFoundPage.module.css";
-
-export const NotFoundPage = () => {
-    return <div className={cls.title}>Not Found Page</div>;
-};
